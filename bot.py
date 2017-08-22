f9b2b38e920a71db26a5e76624678a111f4f09fa
eeeeeeeelias <ilya.pogodaev@outlook.com>
2017-08-22 19:26:26 +0300

    first state-in-database tries

--- bot.py
+++ bot.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 import config
+import sqlite3
 import telebot
 import time
+
+conn = sqlite3.connect('user_states.db')
+cursor = conn.cursor()
 
 bot = telebot.TeleBot(config.token)
 
@@ -14,6 +18,11 @@ def send_about(message):
 @bot.message_handler(commands=['saymyname'])
 def send_user_first_name(message):
     bot.send_message(message.chat.id, 'Hi, ' + message.chat.first_name + '!')
+
+@bot.message_handler(commands=['start'])
+def start_chat(message):
+    bot.send_message(message.chat.id, 'Привет!')
+    cursor.execute('INSERT INTO users (user_id, state) VALUES (' + str(message.chat.id) + ', 0)')
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_msg(message):
