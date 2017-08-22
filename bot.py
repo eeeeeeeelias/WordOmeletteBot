f29c0f73e83f0653ae11ef18d95c60725c2efcf0
eeeeeeeelias <ilya.pogodaev@outlook.com>
2017-08-22 21:53:45 +0300

    added state 0 for /start

--- bot.py
+++ bot.py
@@ -4,8 +4,20 @@ import sqlite3
 import telebot
 import time
 
-conn = sqlite3.connect('user_states.db')
-cursor = conn.cursor()
+class SQLite:
+
+    def __init__(self, database):
+        self.connection = sqlite3.connect(database, isolation_level=None, timeout=5000)
+        self.cursor = self.connection.cursor()
+
+    def new_state(self, chat_id, state):
+        self.cursor.execute('REPLACE INTO users (user_id, state) VALUES (' + str(chat_id) + ', ' + str(state) + ')')
+
+    # def create_new_table(self, table_name):
+    #     self.cursor.execute('CREATE TABLE ' + table_name + '(some_column)')
+
+    def close(self):
+        self.connection.close()
 
 bot = telebot.TeleBot(config.token)
 
@@ -22,7 +34,10 @@ def send_user_first_name(message):
 @bot.message_handler(commands=['start'])
 def start_chat(message):
     bot.send_message(message.chat.id, 'Привет!')
-    cursor.execute('INSERT INTO users (user_id, state) VALUES (' + str(message.chat.id) + ', 0)')
+    db_worker = SQLite(config.database_name)
+    db_worker.new_state(message.chat.id, 0)
+    db_worker.close()
+
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_msg(message):
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
 token = '410912697:AAHPbAS0J3kp_nodtpDprBDD6clXkoXhmrM'
+database_name = 'C:\\dev\\WordOmeletteBot\\user_states.db'
