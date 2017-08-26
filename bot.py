cdbcc67520ac50ff815e3fb2d09a5fd16c0ccd77
eeeeeeeelias <ilya.pogodaev@outlook.com>
2017-08-26 14:54:40 +0300

    added /newword with parameter, checking if user/word exists, stub

--- bot.py
+++ bot.py
@@ -10,38 +10,68 @@ class SQLite:
         self.connection = sqlite3.connect(database, isolation_level=None, timeout=5000)
         self.cursor = self.connection.cursor()
 
-    def new_state(self, chat_id, state):
-        self.cursor.execute('REPLACE INTO users (user_id, state) VALUES (' + str(chat_id) + ', ' + str(state) + ')')
+    def check_if_user_is_familiar(self, chat_id):
+        self.cursor.execute("SELECT COUNT(*) FROM users WHERE user_id = " + str(chat_id))
+        result = self.cursor.fetchone()
+        return result[0]
+
+    def set_new_state(self, chat_id, word):
+        if (state == 0):
+            self.cursor.execute("REPLACE INTO users (user_id, user_state, words_number) VALUES (" + str(chat_id) + ", " + str(state) + ", 0)")
+
+    def check_if_word_is_familiar(self, chat_id, word):
+        self.cursor.execute("SELECT COUNT(*) FROM english_words WHERE english_words.word = \"" + word + "\" AND english_words.user_id = " + str(chat_id))
+        result = self.cursor.fetchone()
+        return result[0]
+
+    def add_new_word(self, chat_id, word, appearance_date):
+        # return "INSERT INTO english_words (word, appearance_date, user_id) VALUES (\"" + word + "\", " + str(appearance_date) + ", " + str(user_id) + ")"
+        self.cursor.execute("INSERT INTO english_words (word, appearance_date, user_id) VALUES (\'" + word + "\', " + str(appearance_date) + ", " + str(chat_id) + ")")
 
     # def create_new_table(self, table_name):
-    #     self.cursor.execute('CREATE TABLE ' + table_name + '(some_column)')
+    #     self.cursor.execute("CREATE TABLE " + table_name + "(some_column)")
+
 
     def close(self):
         self.connection.close()
 
 bot = telebot.TeleBot(config.token)
 
-about_string = 'Этот бот попытается помочь вам подготовиться к сдаче IELTS.'
+about_string = "Этот бот поможет вам учить иностранные слова."
 
-@bot.message_handler(commands=['about'])
+@bot.message_handler(commands=["about"])
 def send_about(message):
     bot.send_message(message.chat.id, about_string)
 
-@bot.message_handler(commands=['saymyname'])
-def send_user_first_name(message):
-    bot.send_message(message.chat.id, 'Hi, ' + message.chat.first_name + '!')
-
-@bot.message_handler(commands=['start'])
-def start_chat(message):
-    bot.send_message(message.chat.id, 'Привет!')
-    db_worker = SQLite(config.database_name)
-    db_worker.new_state(message.chat.id, 0)
+# @bot.message_handler(commands=["newword"])
+@bot.message_handler(regexp="\/newword [A-Za-z \']+")
+def add_new_word(message):
+    new_word = message.text[9:]
+    db_worker = SQLite(config.db_name)
+    # bot.send_message(message.chat.id, "result[0] = " + str(db_worker.check_if_word_is_familiar(message.chat.id, new_word)))
+    if (db_worker.check_if_word_is_familiar(message.chat.id, new_word)):
+        bot.send_message(message.chat.id, "Это слово уже есть в базе!")
+    else:
+        bot.send_message(message.chat.id, "Слово успешно добавлено!")
+        db_worker.add_new_word(message.chat.id, new_word, message.date)
     db_worker.close()
 
+@bot.message_handler(commands=["saymyname"])
+def send_user_first_name(message):
+    bot.send_message(message.chat.id, "Hi, " + message.chat.first_name + "!")
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
+@bot.message_handler(commands=["start"])
+def start_chat(message):
+    db_worker = SQLite(config.db_name)
+    if (db_worker.check_if_user_is_familiar(message.chat.id)):
+        bot.send_message(message.chat.id, "Мы уже знакомы!")
+    else:
+        db_worker.set_new_state(message.chat.id, 0)
+        bot.send_message(message.chat.id, "Привет, " + message.chat.first_name + "! Кажется, мы ещё не знакомы.")
+    db_worker.close()
+
+@bot.message_handler(func=lambda message: True, content_types=["text"])
 def echo_msg(message):
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, "Простите, наш бот это не понимает :(")
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+bot.polling()
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
 token = '410912697:AAHPbAS0J3kp_nodtpDprBDD6clXkoXhmrM'
-database_name = 'C:\\dev\\WordOmeletteBot\\user_states.db'
+db_name = 'C:\\dev\\WordOmeletteBot\\db.db'
+users_db_name = 'C:\\dev\\WordOmeletteBot\\users.db'
+words_db_name = 'C:\\dev\\WordOmeletteBot\\words.db'
